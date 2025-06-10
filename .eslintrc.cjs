3f5d72dffac685be0faa34c881fed4311c6d77c0
whoamaiii <thehitmanfx@gmail.com>
2025-06-10 23:12:07 +0200

    chore(tooling): add ESLint, Prettier, lint-staged and husky pre-commit hooks

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
+  },
+  rules: {
+    'no-console': 'warn',
+  },
+}; 
